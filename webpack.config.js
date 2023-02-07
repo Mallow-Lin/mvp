9020293d9d69472be1b7e0df77fff1750f9790ee
Mallow <linlam0923@gmail.com>
2023-02-07 10:54:55 -0500

    general setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './client/index.js',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './Client/dist'),
     filename: 'index_bundle.js',
   },
   target: 'web',
